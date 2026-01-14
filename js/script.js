b98979f505c57d87fecbb7630891788fdc1244ff
MhZ9191 <hegic91@gmail.com>
2026-01-14 14:24:35 +0100

    Added function generate 5 random numbers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
+//VARIABILI PER IL COUNTDOWN
 const countD = document.getElementById("countdown");
 let counter = 5; //imposta i secondi del countdown
 
+//VARIABILI PER I NUMERI CASUALI
+const numbers = document.getElementById("numbers-list");
+const liArray = generateFiveRandomNumbers(); //assegno un array di 5 numeri random
+
+//VARIABILE INTERVAL
 const intervalID = setInterval(countF, 1000);
 
 countF(); //la chiamo subito
-
-// FUNCTION TO COUNTDOWN
+// FUNCTION INTERVAL COUNTDOWN
 function countF() {
   if (counter >= 0) {
     countD.innerText = counter--;
   } else {
     clearInterval(intervalID);
   }
+}
+
+// FUNZIONE CHE GENERA 5 NUMERI RANDOM TRA 1 E 50
+function generateFiveRandomNumbers() {
+  // non voglio numeri doppi
+  const tmpArray = [];
+  for (let i = 0; i < 5; i++) {
+    let tmpNumber = Math.floor(Math.random() * 50 + 1);
+    while (tmpArray.includes(tmpNumber)) {
+      tmpNumber = Math.floor(Math.random() * 50 + 1);
+    }
+    tmpArray.push(tmpNumber);
+  }
+
+  return tmpArray;
 }
